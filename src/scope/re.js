4e76bd541fc11482a31781fc644b546caed0d6b0
Olivel Cantoran Gonzalez <ocg101010@gmail.com>
2023-02-18 15:45:55 -0600

    reasgnacion

---
+++ src/scope/re.js
@@ -0,0 +1,38 @@
+var firstName; // Undefined
+
+firstName = 'Olivel';
+
+console.log(firstName);
+
+var lastName = 'Olivel2'; // decalrar y asignar
+lastName = 'Cantoran'; // reasignar
+console.log(lastName);
+
+var secondName = 'Cantoran' // declarando y asignando
+var secondName = 'Gonzalez'; // Redeclarar
+console.log(secondName);
+
+// Let
+
+let fruit = 'Apple' // declarar y asignar
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+let fruit = 'Banana';
+console.log(fruit);
+
+// Const
+
+const animal = 'Dog' // declarada y asignada
+animal = 'Cat' // reasignando
+const animal = 'Snake';
+console.log(animal);
+
+
+
+const vehicles = [];
+vehicles.push('Carro')
+
+console.log(vehicles)
+
+vehicles.pop();
+console.log(vehicles)
