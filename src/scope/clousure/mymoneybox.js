d58a6c5f78429f8769aff96f14552edd83d9d5db
Olivel Cantoran Gonzalez <ocg101010@gmail.com>
2023-02-18 22:36:02 -0600

    Hoisting y clousure

---
+++ src/scope/clousure/mymoneybox.js
@@ -0,0 +1,43 @@
+// function moneyBox(coins) {
+//   let saveCoins = 0;
+//   saveCoins += coins;
+//   console.log(`MoneyBox: $${saveCoins}`);
+// }
+
+// moneyBox(5);
+// moneyBox(5);
+
+function moneyBox() {
+  let saveCoins = 0;
+  function countCoins(coins) {
+    saveCoins += coins;
+    console.log(`MoneyBox: $${saveCoins}`);
+  }
+  return countCoins;
+}
+
+const myMoneyBox = moneyBox();
+
+myMoneyBox(5);
+myMoneyBox(5);
+myMoneyBox(15);
+
+
+const moneyBoxAna = moneyBox();
+
+moneyBoxAna(10);
+moneyBoxAna(20);
+moneyBoxAna(5);
+
+
+
+// function createPetList() {
+//   const petList = [];
+//   return function (newPet) {
+//     if (newPet) {
+//       petList.push(newPet);
+//     }
+//     return petList;
+//   };
+// }
+// const myPetList = createPetList();
